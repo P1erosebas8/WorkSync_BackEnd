42c44839178af46fa8921843c6786d3708c72aa3
Pierosebas8 <pierosebas8@hotmail.com>
2026-05-25 23:44:06 -0500

    ultimos cambios

--- src/main/java/com/WorkSync_BackEnd/security/SecurityConfig.java
+++ src/main/java/com/WorkSync_BackEnd/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
